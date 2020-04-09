57f886a9edbd03abf71b3e34a98514765132646b
nuthanc <nuthanc@juniper.net>
2020-04-09 10:24:04 -0700

    Add error log

--- cc-charm-2005/cmd.sh
+++ cc-charm-2005/cmd.sh
@@ -1,4 +1,6 @@
-juju deploy ./tf-charms/contrail-command --to g20 --config docker-registry='bng-artifactory.juniper.net/contrail-nightly' --config image-tag=master.1159
+# juju deploy ./tf-charms/contrail-command --to 3 --config docker-registry='bng-artifactory.juniper.net/contrail-nightly' --config image-tag=master.1159
+juju deploy ./tf-charms/contrail-command --constraints "tags=g20" --config docker-registry='bng-artifactory.juniper.net/contrail-nightly' --config image-tag=master.1159
+
 juju status
 juju add-relation contrail-command contrail-controller
 juju status
@@ -9,9 +11,11 @@ cat cert.pem | base64 > "cert.pem.b64"
 # Need to create config file
 cat config.yaml 
 
-juju run-action contrail-command/0 import-cluster --params config.yaml
-# Action queued with id: "65"
+juju run-action contrail-command/2 import-cluster --params config.yaml
+# Action queued with id: "3"
 juju status
-juju show-action-status 65
+juju show-action-status 4
 juju status
-juju show-action-output 65 | grep result
+juju show-action-output 4 | grep result
+
+# juju remove-application contrail-command --force
