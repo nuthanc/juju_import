dc962eb4682ed89930ed64f13d93520c981ea965
nuthanc <nuthanc@juniper.net>
2020-04-13 04:41:28 -0700

    Update cmd

--- cc-charm-2005/cmd.sh
+++ cc-charm-2005/cmd.sh
@@ -1,9 +1,6 @@
 # juju deploy ./tf-charms/contrail-command --to 3 --config docker-registry='bng-artifactory.juniper.net/contrail-nightly' --config image-tag=master.1159
 juju deploy ./tf-charms/contrail-command --constraints "tags=g20" --config docker-registry='bng-artifactory.juniper.net/contrail-nightly' --config image-tag=master.1159 --config docker-registry-insecure=true
-
-juju status
 juju add-relation contrail-command contrail-controller
-juju status
 juju change-user-password
 juju show-controller
 # Copy certificate to cert.pem
@@ -13,7 +10,7 @@ cat config.yaml
 
 juju run-action contrail-command/1 import-cluster --params config.yaml
 # Action queued with id: "3"
-juju status
+
 juju show-action-status 3
 juju status
 juju show-action-output 1 | grep result
