03bf81437b0634bf4a707e10fe06f95cec8d8c34
nuthanc <nuthanc@juniper.net>
2020-04-22 03:50:38 -0700

    UPdate py_parse.py

--- cc-charm-2005-auto/out_check.py
+++ cc-charm-2005-auto/out_check.py
@@ -1,10 +1,20 @@
 import os
 import yaml
+import subprocess
 
-with open('/root/juju_import/cc-charm-2005-auto/controller.yml') as file:
-    controller = yaml.load(file, Loader=yaml.FullLoader)
-    cert = controller['myjujucontroller']['details']['ca-cert']
-    with open('cert.pem','w') as file:
-      file.write(cert)
+def gen_cert_and_b64():
+  """
+  Take controller.yml as input and generate cert.pem and cert.pem.b64
+  """
+  with open('/root/juju_import/cc-charm-2005-auto/controller.yml') as file:
+      controller = yaml.load(file, Loader=yaml.FullLoader)
+      cert = controller['myjujucontroller']['details']['ca-cert']
+      with open('cert.pem','w') as file:
+        file.write(cert)
+  os.system("cat cert.pem | base64 > 'cert.pem.b64'")
+# End of gen_cert_and_b64
 
-os.system("cat cert.pem | base64 > 'cert.pem.b64'")
+# os.system("juju status")
+# os.system("cat cmd.sh")
+out = subprocess.run(["juju", "status"],stdout=subprocess.PIPE, universal_newlines=True)
+print(out.stdout)
--- cc-charm-2005-auto/py_parse.py
+++ cc-charm-2005-auto/py_parse.py
@@ -2,17 +2,22 @@ import yaml
 import base64
 import pexpect
 import os
+import time
 
-os.system("juju show-controller > controller.yml")
-try:
-  change_command = pexpect.spawn('juju change-user-password')
-  change_command.expect('new password: ', timeout=5)
-  change_command.sendline('c0ntrail123')
-  change_command.expect('type new password again: ', timeout=5)
-  change_command.sendline('c0ntrail123')
-except Exception as e:
-  print(str(e))
-else:
+def change_user_password():
+  try:
+    change_command = pexpect.spawn('juju change-user-password')
+    change_command.expect('new password: ', timeout=5)
+    change_command.sendline('c0ntrail123')
+    change_command.expect('type new password again: ', timeout=5)
+    change_command.sendline('c0ntrail123')
+  except Exception as e:
+    print(str(e))
+
+
+def prepare_config_file():
+  os.system("juju show-controller > /root/juju_import/cc-charm-2005-auto/controller.yml")
+
   with open('/root/juju_import/cc-charm-2005-auto/controller.yml') as file:
     controller = yaml.load(file, Loader=yaml.FullLoader)
     juju_config = {}
@@ -24,5 +29,22 @@ else:
     juju_config['juju-model-id'] = controller['myjujucontroller']['models']['default']['model-uuid']
     juju_config['juju-controller-password'] = 'c0ntrail123'
 
-  with open('/root/juju_import/cc-charm-2005-auto/auto_config.yml', 'w') as file:
-    documents = yaml.dump(juju_config, file)
+    with open('/root/juju_import/cc-charm-2005-auto/config.yaml', 'w') as file:
+      documents = yaml.dump(juju_config, file)
+
+
+def deploy():
+  os.system('juju deploy ./tf-charms/contrail-command --constraints "tags=g20" --config docker-registry='bng-artifactory.juniper.net/contrail-nightly' --config image-tag=master.1167 --config docker-registry-insecure=true')
+  time.sleep(2)
+  os.system("juju add-relation contrail-command contrail-controller")
+  time.sleep(2)
+  os.system("juju run-action contrail-command/0 import-cluster --params config.yaml")
+  os.system("juju show-action-status 1")
+
+
+if __name__ == '__main__':
+  change_user_password()
+  prepare_config_file()
+  deploy()
+
+  
